a07a7d44dba6d18c9590c61379081f7e61b7a50f
sarker24 <sarker24@gmail.com>
2024-01-14 23:34:20 +0100

    Add-Dsahboard-Sidebar

---
+++ src/layout/PrimaryLayout.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Inter } from "next/font/google";
+import { usePathname } from "next/navigation";
+import Footer from "./Footer";
+import NavbarDefault from "./Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function PrimaryLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  const noNav = ["/signup", "/signin"];
+
+  if (noNav.includes(pathname)) {
+    return (
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    );
+  }
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NavbarDefault />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
