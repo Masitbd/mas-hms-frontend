e276b39b7bce11187988c8b1fa5603cd94379ad1
maruf9270 <maruf9270@gmail.com>
2024-01-24 13:10:47 +0600

    Optimized pdrv code

--- src/app/(withlayout)/pdrv/page.tsx
+++ src/app/(withlayout)/pdrv/page.tsx
@@ -1,12 +1,9 @@
 "use client";
-import NewPdrvModal from "@/components/pdrv/NewPdrvModal";
+import NewPdrv from "@/components/pdrv/NewPdrv";
 import PdrvTable from "@/components/pdrv/PdrvTable";
-import { usePostConditionMutation } from "@/redux/api/condition/conditionSlice";
-import { usePostPdrvMutation } from "@/redux/api/pdrv/pdrvSlice";
-import { setLoading } from "@/redux/features/loading/loading";
-import React, { use, useEffect, useState } from "react";
-import { Button, toaster, Message } from "rsuite";
-export type IConditon = {
+import React, { useState } from "react";
+import { Button } from "rsuite";
+export type IPdrv = {
   _id?: string;
   label: string;
   value: string;
@@ -14,37 +11,10 @@ export type IConditon = {
 };
 const Pdrv = () => {
   const [postModalOpen, setPostModalOpen] = useState(false);
-  const [postPdrv, { isError, isLoading: conditionLoading, isSuccess, error }] =
-    usePostPdrvMutation();
   const cancelHandlerforPost = () => {
     setPostModalOpen(!postModalOpen);
   };
 
-  useEffect(() => {
-    if (isSuccess) {
-      toaster.push(<Message type="success">Pdrv Added Successfully</Message>, {
-        duration: 3000,
-      });
-      setPostModalOpen(!postModalOpen);
-      setLoading();
-    }
-    if (conditionLoading) {
-      setLoading();
-    }
-
-    if (isError) {
-      setLoading();
-      toaster.push(
-        <Message type="error">
-          Something went wrong please try again letter
-        </Message>,
-        {
-          duration: 3000,
-        }
-      );
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [conditionLoading, isError, isSuccess]);
   return (
     <div className="my-5 px-5">
       <div className="my-4">
@@ -57,11 +27,13 @@ const Pdrv = () => {
       </div>
 
       <div>
-        <NewPdrvModal
-          postPdrv={postPdrv}
+        <NewPdrv
+          modalStatusHandler={setPostModalOpen}
           open={postModalOpen}
           cancelHandler={cancelHandlerforPost}
-        ></NewPdrvModal>
+        ></NewPdrv>
+      </div>
+      <div>
         <PdrvTable></PdrvTable>
       </div>
     </div>
---
+++ src/components/ui/Modal.tsx
@@ -0,0 +1,41 @@
+import { useAppSelector } from "@/redux/hook";
+import React from "react";
+import { Button, Loader, Modal } from "rsuite";
+
+const RModal = ({
+  size,
+  open,
+  title,
+  children,
+  okHandler,
+  cancelHandler,
+}: {
+  size: string;
+  open: boolean;
+  title: string;
+  children: React.ReactElement;
+  okHandler: <T>(data: T) => void;
+  cancelHandler: <T>(data: T) => void;
+}) => {
+  const loading = useAppSelector((state) => state.loading.loading);
+  return (
+    <div>
+      <Modal size={size} open={open}>
+        <Modal.Header>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="p-5">{children}</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={cancelHandler} appearance="subtle">
+            Cancel
+          </Button>
+          <Button onClick={okHandler} appearance="primary">
+            {loading ? <Loader></Loader> : "OK"}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default RModal;
