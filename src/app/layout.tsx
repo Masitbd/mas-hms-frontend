bf0da4f7ffb58445dd1f9795fcc3496e29a3d0e9
git <maruf9270@gmail.com>
2024-06-04 05:45:23 +0600

    l

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
-"use client";
 import PrimaryLayout from "@/layout/PrimaryLayout";
 import "./globals.css";
 import type { Metadata } from "next";
 import Providers from "@/lib/Providers";
 
-// export const metadata: Metadata = {
-//   title: "HMS",
-//   description: "Hospital Management Service",
-// };
+export const metadata: Metadata = {
+  title: "HMS",
+  description: "Hospital Management Service",
+  icons: [
+    { rel: "icon", url: "/favicon-32x32.png" },
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png" },
+    { rel: "manifest", url: "/site.webmanifest" },
+    { rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#5bbad5" },
+  ],
+};
 
 export default function RootLayout({
   children,
