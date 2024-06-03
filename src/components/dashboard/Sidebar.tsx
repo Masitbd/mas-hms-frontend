6adc6a6e0f64389e5c9ab0cb7174427fbbcc0da6
git <maruf9270@gmail.com>
2024-06-03 15:32:22 +0600

    Fixed some auth issue

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -36,6 +36,11 @@ const NavToggle = ({ expand, onChange }: NavToggleProps) => {
     </Navbar>
   );
 };
+const userMenuItem = [
+  { key: "1", title: "Users", href: "/users" },
+  { key: "2", title: "Prifile", href: "/profile" },
+  { key: "3", title: "Permissions", href: "/permission" },
+];
 
 const testMenuItem = [
   { key: "1", title: "Pdrv", href: "/pdrv" },
@@ -47,16 +52,6 @@ const testMenuItem = [
   { key: "7", title: "Vacumme Tube", href: "/vacuumTube" },
   { key: "8", title: "Test", href: "/test" },
   { key: "9", title: "Doctor", href: "/doctor" },
-  {
-    key: "10",
-    title: "Patient",
-    href: "/patient",
-  },
-  {
-    key: "11",
-    title: "Generate Bill",
-    href: "/order",
-  },
 ];
 
 const Sidebar = () => {
@@ -108,13 +103,34 @@ const Sidebar = () => {
                 ))}
               </Nav.Menu>
               <Nav.Menu
-                eventKey="4"
+                eventKey="5"
                 trigger="hover"
-                title="Settings"
+                title="Manage Users"
                 icon={<GearCircleIcon />}
                 placement="rightStart"
               >
-                <Nav.Item eventKey="4-5">Versions</Nav.Item>
+                {userMenuItem.map((item) => (
+                  <Nav.Item
+                    eventKey={`5-${item.key}`}
+                    href={item.href}
+                    as={NavLink}
+                    key={Number(item.key) + 20}
+                    onClick={() => setSelectedItem(item.href)}
+                    style={
+                      selectedItem === item.href
+                        ? {
+                            backgroundColor: "#3498ff",
+                            color: "white",
+                            borderRadius: "5px",
+                          }
+                        : {
+                            color: "black",
+                          }
+                    }
+                  >
+                    {item.title}
+                  </Nav.Item>
+                ))}
               </Nav.Menu>
             </Nav>
           </Sidenav.Body>
