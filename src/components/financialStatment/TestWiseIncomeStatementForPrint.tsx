7d5e304efa9655fa1b92c9b9b0f61a2bd1b5915b
git <maruf9270@gmail.com>
2024-09-16 00:57:57 +0600

    d

--- src/components/financialStatment/TestWiseIncomeStatementForPrint.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export class ComponentToPrint extends React.PureComponent {
-  render() {
-    const porps = this.props;
-    return <div>My cool content {this.props}</div>;
-  }
-}
