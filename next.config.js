8024ba26aa5ffafb69793583320947081295c60c
Maruf-9270 <marufahmed9270@gmail.com>
2025-06-01 07:25:42 +0600

    Updated for gopalganj Client

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "/ddcg/diagnostic",
-  // assetPrefix: "/ddcg/diagnostic",
+  basePath: "/ddcg/diagnostic",
+  assetPrefix: "/ddcg/diagnostic",
 
   images: {
     remotePatterns: [
