a842bf7cb80850cc201397625a801ce482e2b2d1
Maruf-9270 <marufahmed9270@gmail.com>
2025-07-12 12:17:45 +0600

    changed nav

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -43,7 +43,11 @@ const CustomNavbar = ({
         <Nav.Item eventKey="3" as={NavLink} href="/order">
           Dashboard
         </Nav.Item>
-        <Nav.Item eventKey="4" as={NavLink} href="http://148.135.137.151:3005">
+        <Nav.Item
+          eventKey="4"
+          as={NavLink}
+          href="http://148.135.137.151/ddcg/account/"
+        >
           Account
         </Nav.Item>
       </Nav>
